f92d14b79dd0756a4755411fe7e39244ad4f93d0
des03046 <73879718+des03046@users.noreply.github.com>
2020-12-24 15:34:23 +0900

    Add files via upload

---
+++ TaxicabGeometry.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TaxicabGeometry {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+
+		System.out.println(r * r * Math.PI);
+		System.out.println(r * r * 2);
+	}
+}//https://www.acmicpc.net/problem/3053
