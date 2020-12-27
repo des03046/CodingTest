4640396dfdf44606a12aff2406d814639b89e75a
des03046 <73879718+des03046@users.noreply.github.com>
2020-12-27 16:27:45 +0900

    Add files via upload

---
+++ SieveOfEratosthenes.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SieveOfEratosthenes {
+	public static boolean[] prime;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		solution(b);
+		for (int i = a; i <= b; i++) {
+			if (prime[i] == false) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	public static void solution(int n) {
+		prime = new boolean[n + 1];
+		prime[0] = prime[1] = true;
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (prime[i] == true)
+				continue;
+			for (int j = i; i * j < prime.length; j = j + 1) {
+				prime[i*j] = true;
+			}
+		}
+	}
+}
+
