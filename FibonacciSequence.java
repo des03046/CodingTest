5fcdde6be8d162c7cedf0fd57430f896120665ff
des03046 <73879718+des03046@users.noreply.github.com>
2020-12-27 16:45:17 +0900

    Add files via upload

---
+++ FibonacciSequence.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FibonacciSequence {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(solution(a));
+	}
+
+	public static int solution(int n) {
+		if (n <= 0) {
+			return 0;
+		}
+		if (n == 1) {
+			return 1;
+		} else
+			return solution(n - 1) + solution(n - 2);
+	}
+}
