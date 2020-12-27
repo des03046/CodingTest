270bca424e3ffcb263913cd134f19b048fc59c8c
des03046 <73879718+des03046@users.noreply.github.com>
2020-12-27 16:34:24 +0900

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println(solution(a));
+	}
+
+	public static int solution(int n) {
+		if (n <= 1) {
+			return 1;
+		} else
+			return solution(n - 1) * n;
+	}
+}
